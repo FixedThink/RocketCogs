fb769b22435f3ed02473dbfa51364651e02a9b81
HRAND5 <35737340+HRAND5@users.noreply.github.com>
2019-02-16 20:54:58 +0100

    Added [p]leaderboard command

--- reputation/reputation.py
+++ reputation/reputation.py
@@ -6,6 +6,7 @@ import discord
 from redbot.core import commands
 from redbot.core import checks, Config, data_manager
 from redbot.core.bot import Red
+import redbot.core.utils.menus as red_menu
 
 # Local files.
 from .db_queries import DbQueries
@@ -21,7 +22,7 @@ class Reputation(commands.Cog):
     DONE = ":white_check_mark: "
 
     DEFAULT_COOLDOWN = 60 * 60 * 24 * 7  # 1 week (cooldown for user A to give user B rep).
-    DEFAULT_DECAY = 60 * 60 * 24 * 7 * 5  # 5 weeks (35 days, time before the reputation role will decay). 
+    DEFAULT_DECAY = 60 * 60 * 24 * 7 * 5  # 5 weeks (35 days, time before the reputation role will decay).
     BAD_CHANNEL = ERROR + "Reputation not added, please use the correct channel for reputations!"
     CHANNEL_CLEARED = BIN + "Cleared the channel configuration. Reputation can now be given in any channel."
     CHANNEL_SET = DONE + "Set the reputation channel to {}."
@@ -34,7 +35,9 @@ class Reputation(commands.Cog):
     REP_YOURSELF = ERROR + "Loving yourself is great, but giving yourself reputation is a bit extreme."
     COUNT_DESC = "{} has received **{}** reputation{} from **{}** user{}."
     COUNT_NO_REPS = "{} has not received any reputations."
-    
+    LEADERBOARD_NO_REPS = ERROR + "No reputations in the database."
+    LEADERBOARD_ROW = "`{}` {} - {} reps"
+
     def __init__(self, bot: Red):
         super().__init__()
         self.bot = bot
@@ -58,11 +61,11 @@ class Reputation(commands.Cog):
         """Configure the reputation commands"""
         await ctx.send_help()  # TODO: Add codeblock that shows current config.
 
+    @commands.guild_only()
     @checks.admin_or_permissions(administrator=True)
     @_reputation_settings.command(name="channel")
     async def set_rep_channel(self, ctx):
         """Set the reputation channel
-
         The reputation channel will be set to the channel in which this command is executed.
         If this channel is already the reputation channel, the config will be cleared."""
         channel = ctx.channel
@@ -79,9 +82,7 @@ class Reputation(commands.Cog):
     @_reputation_settings.command(name="cooldown")
     async def set_rep_cooldown(self, ctx, days: int, hours: int = 0, minutes: int = 0, seconds: int = 0):
         """Set the reputation cooldown
-
         If a cooldown is active, user A cannot give user B a reputation for the set period after the last rep pair.
-
         You must use the days argument. If the time provided equals zero, the config will be set to None.
         If the time provided < 0, then the config will be reset to its default (1 week).
         """
@@ -103,7 +104,6 @@ class Reputation(commands.Cog):
     @commands.command()
     async def rep(self, ctx, user: discord.Member, *, comment: str = None):
         """Give someone reputation
-
         You may add a comment, but this is not necessary."""
         # TODO: Possibly restrict length of rep message.
         aut = ctx.author
@@ -146,8 +146,52 @@ class Reputation(commands.Cog):
             embed.description = self.COUNT_NO_REPS.format(user.mention)
         await ctx.send(embed=embed)
 
+    @commands.command(name="leaderboard")
+    async def rep_leaderboard(self, ctx):
+        lb_data = await self.rep_db.rep_leaderboard()
+
+        if lb_data is None:
+            await ctx.send(self.LEADERBOARD_NO_REPS)
+        else:  # At least one rep given
+            lb_length = len(lb_data)
+            desc = "Total users that have received at least 1 reputation: **{}**".format(lb_length)
+
+            # Split the leaderboard into fields with under 10 rows
+            field_list = []
+            for i in range((lb_length // 10) + 1):
+                start = 10 * i
+                end = start + 10 if lb_length > (start + 10) else lb_length
+
+                field_name = "{}-{}".format(start + 1, end)
+                field_value = "\n".join((self.LEADERBOARD_ROW.format((i + 1), self.mention_from_id(t[0]), t[1])
+                                         for i, t in enumerate(lb_data[start:end], start = start)))
+                field_list.append((field_name, field_value))
+
+            embed_list = []
+            field_count = len(field_list)
+            if field_count == 1:  # If only 1 field, send as 1 embed
+                f_name = field_list[0][0]
+                f_value = field_list[0][1]
+                embed = discord.Embed(title="Reputation leaderboard", description=desc, colour=discord.Colour.purple())
+                embed.add_field(name=f_name, value=f_value)
+                footer = "Page 1 out of 1."
+                embed.set_footer(text=footer)
+                await ctx.send(embed=embed)
+            else:  # If more than one field, send as a pagified menu.
+                for n, (f_name, f_value) in enumerate(field_list, start=1):
+                    embed = discord.Embed(title="Reputation leaderboard", description=desc, colour=discord.Colour.purple())
+                    embed.add_field(name=f_name, value=f_value)
+                    footer = "Page {n} out of {total}.".format(n=n, total=field_count)
+                    embed.set_footer(text=footer)
+                    embed_list.append(embed)
+                await red_menu.menu(ctx, embed_list, red_menu.DEFAULT_CONTROLS, timeout=30.0)
+
     # Utilities
     @staticmethod
     def plural_s(n: int) -> str:
         """Returns an 's' if n is not 1, otherwise returns an empty string"""
         return "" if n == 1 else "s"
+
+    @staticmethod
+    def mention_from_id(user_id: int) -> str:
+        return "<@" + str(user_id) + ">"
